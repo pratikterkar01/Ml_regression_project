9003cc2b9db38639e59767b12e43b1a23af8e4ae
pratikterkar <pratikterkar01@gmail.com>
2023-05-10 21:00:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+##end toend ml project
+## create environment
+
+''' conda create -p venv python==3.8
+
+conda activate "path_of_venv"
+ '''
+
+##install all package fron requirment.txt
+
+''' pip install -r requirment.txt '''
+##create src under which all ml project run
+''' inside that logging,exception,handling ans utils are create utils having any generic funtionality that wont to create '''
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List 
+
+
+HYPHEN_E_DOT="-e ."
+
+
+
+def get_requirments(file_path:str)->List[str]:
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments=file_obj.readlines()
+        [req.replace("\n","")for req in requirments]
+    
+    if HYPHEN_E_DOT in requirments:
+        requirments.remove(HYPHEN_E_DOT)
+    
+    
+    return requirments
+
+
+setup(
+    name="regressionp _project",
+    version="0.0.1",
+    author="pratik",
+    author_email="pratikterkar@gmail.com",
+    install_requires=get_requirments("requirments.txt"),
+    packages=find_packages()
+)
